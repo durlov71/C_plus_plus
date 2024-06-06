9d27949f2cc864d828517bc45b6f249c4c936153
Dark <istiak.csinfo@gmail.com>
2024-06-07 01:57:17 +0600

    basic

---
+++ Setup/test.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    float b= 10.80;
+    std::cout << "New number "<< a<<std::endl<<"my float number is "<<b;
+    return 0;
+}
